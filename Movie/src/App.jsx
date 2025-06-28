77cf0e3ff33552cea9e2f241d1ae04fdb4ad9885
Pranav Deshmukh <155602302+Mechdeta@users.noreply.github.com>
2025-06-28 16:44:26 +0000

    todays work

---
+++ Movie/src/App.jsx
@@ -0,0 +1,19 @@
+const Card = () => {
+  return(
+    <div>
+    <h2>Card Component</h2>
+    </div>
+  )
+}
+
+
+const App = () => {
+    return (
+      <div>
+      <h2>Functional Arrow component</h2>
+      <Card />
+      </div>
+    )
+}
+
+export default App
